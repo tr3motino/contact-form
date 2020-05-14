00b16e84cf861aa812f9ff936ee0455d33112ec8
tr3motino <nino@grafikbuam.de>
2020-05-14 16:01:32 +0200

    fix config, redirect & bug datetime

--- src/config/general.php
+++ src/config/general.php
@@ -4,4 +4,28 @@ return [
     '*' => [
         'useProjectConfigFile' => true,
     ],
+
+    return [
+        'ssoUrl' => $env === 'prod'
+          ? 'https://sso.comjoo.com/auth/realms/comjoo-hub/protocol/openid-connect/token'
+          : 'https://sso.comjoo.net/auth/realms/comjoo-hub/protocol/openid-connect/token',
+        'ssoCredentials' => [
+          'form_params' => [
+               'client_id' => 'sanecum-order-form',
+               'grant_type' => 'password',
+               'client_secret' => (($env === 'prod')
+                    ? '49e76eb4-cd11-425f-bef6-6633f2069807'
+                    : 'a0826724-365b-4a47-badb-88ff96e932a1'),
+               'scope=openid',
+               'username' => getenv('JOOBN'),
+               'password' => getenv('JOOPW'),
+          ]],
+        'pmApiRegistrationUrl' => (($env === 'prod')
+          ? "https://pm-doc.comjoo.com/onboarding/forCoronaRecipe"
+          : (($env === 'dev')
+               ? "https://pm-doc.api.comjoo.net/onboarding/forCoronaRecipe"
+               # in docker the clientIp is the host-adress where the ts-node srv is running
+               : "http://host.docker.internal:3335/onboarding/forCoronaRecipe"))
+    ];
+
 ];
--- src/controllers/SendController.php
+++ src/controllers/SendController.php
@@ -7,8 +7,15 @@ use craft\orderform\models\Submission;
 use craft\orderform\Plugin;
 use craft\web\Controller;
 use craft\web\UploadedFile;
+use yii\db\Exception;
 use yii\web\Response;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Psr7;
+use \DateTime;
 
 
 class SendController extends Controller
@@ -24,11 +31,19 @@ class SendController extends Controller
     // Public Methods
     // =========================================================================
 
+    private function getConfig() {
+        #FIXME bitte die config irgendwie auslagern. je nach typ kannst du hier simulieren: local/dev/prod
+       #  use local for local dev; dev for the dev-server; prod for the prod-server
+
+
+    }
+
+
     /**
      * Sends a contact form submission.
      *
-     * @return Response|null
-     */
+     * @return Response|null*/
+
     public function actionIndex()
     {
         $this->requirePostRequest();
@@ -39,30 +54,93 @@ class SendController extends Controller
         $submission = new Submission();
         $submission->firstName = $request->getBodyParam('firstName');
         $submission->lastName = $request->getBodyParam('lastName');
+        $gender = $request->getBodyParam('gender');
+        $submission->gender = array_shift($gender);
+
+
+        $date = $request->getBodyParam('birthday');
+        $date2 = date("Y-m-d H:i:s", strtotime($date));
+        $submission->birthday = $date2;
+
+
         $submission->email = $request->getBodyParam('email');
-        $submission->birthday = $request->getBodyParam('birthday');
+        $content = $submission->toArray();
+        $causeType = $request->getBodyParam('causeType');
+        $content['causeType'] = array_shift($causeType);
 
-        $shit = json_encode($submission);
-/*
-        if (!$plugin->getMailer()->send($submission)) {
-            if ($request->getAcceptsJson()) {
-                return $this->asJson(['errors' => $submission->getErrors()]);
-            }
+        $datetime = new DateTime();
+        $content['dataProtection'] = [
+            'agbData' => $request->getBodyParam('agbData') ? 'check':'',
+            'agbEHealth' => $request->getBodyParam('agbEHealth') ? 'check':'',
+            'agbPush' => $request->getBodyParam('agbPush') ? 'check':'',
+            'timeStamp' => $datetime->format(DateTime::ATOM),
+            'clientIp' => $this->getRealIpAddr()
+        ];
 
-            Craft::$app->getSession()->setError(Craft::t('contact-form', 'There was a problem with your submission, please check the form and try again!'));
-            Craft::$app->getUrlManager()->setRouteParams([
-                'variables' => ['message' => $submission]
-            ]);
+        #$config = $this->getConfig();
+        $config = include("/www/htdocs/w019bd37/sanecum-group/order-form/src/config/joo.php");
+        $result = $this->postData($config['ssoUrl'], $config['ssoCredentials']);
+        $token = $result['access_token'];
 
-            return null;
-        }
-*/
-        // return null;
+        $headers = ['headers' => ['Authorization' => 'Bearer ' . $token]];
+        $result = $this->postData($config['pmApiRegistrationUrl'], ['json' => $content], $headers);
+
         if ($request->getAcceptsJson()) {
             return $this->asJson(['success' => true]);
         }
 
         Craft::$app->getSession()->setNotice($settings->successFlashMessage);
-        return $this->redirectToPostedUrl($submission);
+        return null;
+ }
+
+    private function postData($url, $data, $headers = [])
+    {
+        $client = new Client($headers);
+        try {
+            $response = $client->request('POST', $url, $data);
+            $contentType = $response->getHeader('Content-Type');
+            if (!is_bool(strpos($contentType[0], 'application/json'))) {
+                $result = json_decode($response->getBody(), true);
+                return $result;
+            } else {
+                echo "wrong server response",
+                $response->getBody();
+            }
+        } catch (ClientException $e) {
+            error_log (Psr7\str($e->getRequest()));
+            error_log (Psr7\str($e->getResponse()));
+        } catch (TransferException $e) {
+            error_log (Psr7\str($e->getRequest()));
+            if ($e->hasResponse()) {
+                error_log (Psr7\str($e->getResponse()));
+            }
+        } catch (RequestException $e) {
+            error_log (Psr7\str($e->getRequest()));
+            if ($e->hasResponse()) {
+                error_log (Psr7\str($e->getResponse()));
+            }
+        } catch (BadResponseException $e) {
+            error_log (Psr7\str($e->getRequest()));
+            if ($e->hasResponse()) {
+                error_log (Psr7\str($e->getResponse()));
+            }
+        }
+    }
+
+    private function getRealIpAddr()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+        {
+            $ip=$_SERVER['HTTP_CLIENT_IP'];
+        }
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+        {
+            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else
+        {
+            $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
     }
 }
